ff90e5521eb3f6225ae2442b31b450391f17a4fc
OpticWafare <eller.martin29mail.com>
2018-04-11 08:03:20 +0200

    Dummywerte in File speichern

--- src/model/DummyWerteErzeugen.java
+++ src/model/DummyWerteErzeugen.java
@@ -8,64 +8,52 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
 
-public class DummyWerteErzeugen {
+public class DummyWerteErzeugen{
 	static int id = 0;
 	static int zustand = 0;
 	static Timestamp begin = new Timestamp(System.currentTimeMillis());
-	private static final String HOST = "localhost";
-	private static final String SERVER = "jdbc:mysql://";
-	private static final String PORT = "3306";
-	private static final String DB_NAME = "postkasten";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "";
 
-	private static Connection con;
-
-	public static Connection getConnection() {
-
-		if (con == null) {
-			try {
-				con = DriverManager.getConnection(
-						SERVER + HOST + ":" + PORT + "/" + DB_NAME + "?user=" + USERNAME + "&password=" + PASSWORD);
-			} catch (SQLException e) {
-
-				e.printStackTrace();
-			}
-
-		}
-		return con;
-	}
 
 	
-	public static void werteErzeugen(int id, int zustand, Timestamp begin) {
+
+	public static void main(String[] args) throws IOException {
+		werteErzeugen(id, zustand, begin);
+		
+	}
+	public static void werteErzeugen(int id, int zustand, Timestamp begin) throws IOException {
+		
 		for (int i = 0; i < 5; i++) {
+			try {
+				TimeUnit.SECONDS.sleep((int) (Math.random() * 10));
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
 			zustand = (int) (Math.random() * 2);
 			System.out.println("ID: " + id + " Zustand: " + zustand + " Datum und Uhrzeit: " + begin);
-			id++;
+			
 
-			if (i == 0) {
-				insertValues(id, zustand, begin);
+			if (zustand == 0) {
+				FileWriter writer;
+				File datei = new File("PostB.txt");
+				
+				try{
+					writer = new FileWriter(datei, true);
+					writer.write("ID: " + id + " Zustand: " + zustand + " Datum und Uhrzeit: " + begin);
+					writer.write(System.getProperty("line.separator"));
+					
+					writer.flush();
+					writer.close();
+					
+				}catch(IOException e){
+					e.printStackTrace();
+				}	
 			}
+			id++;
 		}
 	}
 
-	public static void insertValues(int id, int zustand, Timestamp begin) {
-
-		String url = SERVER;
-		String table = "Insert into oeffnenundschliessen (id, zeitpunkt, zustand) values(?,?,?);";
-		try (Connection con = DriverManager.getConnection(url); PreparedStatement pstm = con.prepareStatement(table)) {
-			pstm.setInt(1, id);
-			pstm.setTimestamp(2, begin);
-			pstm.setInt(3, zustand);
-			pstm.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
 	
-	public static void main(String[] args) {
-		werteErzeugen(id, zustand, begin);
-	}
-
 }
